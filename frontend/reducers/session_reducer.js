db524a8dbea01a9b191a8a33e8534bfb43f6e786
App Academy Student <pcloeter@gmail.com>
2019-01-21 16:21:12 -0500

    Complete users reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,6 +3,7 @@ import merge from 'lodash'
 
 
 const sessionReducer = (oldState = _nullUser, action) => {
+  Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return {id: [action.currectUser.id]};
